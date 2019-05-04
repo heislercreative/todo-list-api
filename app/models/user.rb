022c4f8d869d9b1343fd05b31259d94b326a4ace
heislercreative <brennan@heislercreative.com>
2019-05-04 14:09:16 -0600

    write & pass User model validation specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ApplicationRecord
   has_many :projects
   has_many :tasks, through: :projects
+  has_secure_password
+
+  before_validation :downcase_email
+  validates_presence_of :email, :password, :first_name, :last_name
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  validates :first_name, format: { with: /\A[a-z]+\z/i }
+  validates :last_name, format: { with: /\A[a-z]+\z/i }
+
+  private
+  # Downcase email characters before validating and persisting
+  def downcase_email
+    self.email.downcase! if self.email
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:attributes) do
+    {
+      email: 'test@test.com',
+      password: 'test123',
+      first_name: 'John',
+      last_name: 'Doe'
+    }
+  end
+
+  let(:missing_email) { attributes.except(:email) }
+  let(:email_no_at) { attributes.merge(email: 'testtest.com') }
+  let(:email_no_period) { attributes.merge(email: 'test@testcom') }
+  let(:email_mixed_case) { attributes.merge(email: 'Test@test.com') }
+  let(:missing_password) { attributes.except(:password) }
+  let(:short_password) { attributes.merge(password: 'test1') }
+  let(:missing_first_name) { attributes.except(:first_name) }
+  let(:first_name_numbers) { attributes.merge(first_name: 'Jo4n') }
+  let(:missing_last_name) { attributes.except(:last_name) }
+  let(:last_name_numbers) { attributes.merge(first_name: 'Do3') }
+
+
+
+context 'email' do
+    it 'should be present' do
+      expect(User.new(missing_email)).to be_invalid
+    end
+
+    it 'should be a valid email address' do
+      expect(User.new(email_no_at)).to be_invalid
+      expect(User.new(email_no_period)).to be_invalid
+    end
+
+    it 'should be a unique email address' do
+      User.create(attributes)
+      expect(User.new(attributes)).to be_invalid
+    end
+
+    it 'should be lowercased' do
+      user = User.create(email_mixed_case)
+      expect(user.email).to eq('test@test.com')
+    end
+  end
+
+
+  context 'password' do
+    it 'should be present' do
+      expect(User.new(missing_password)).to be_invalid
+    end
+
+    it 'should be at least 6 characters long' do
+      expect(User.new(short_password)).to be_invalid
+    end
+  end
+
+
+  context 'first name' do
+    it 'should be present' do
+      expect(User.new(missing_first_name)).to be_invalid
+    end
+
+    it 'should not have numbers' do
+      expect(User.new(first_name_numbers)).to be_invalid
+    end
+  end
+
+  context 'last name' do
+    it 'should be present' do
+      expect(User.new(missing_last_name)).to be_invalid
+    end
+
+    it 'should not have numbers' do
+      expect(User.new(last_name_numbers)).to be_invalid
+    end
+  end
 end
