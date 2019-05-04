8d04599abf1ef7c8ad3e8801f6d89ba6a79d5446
heislercreative <brennan@heislercreative.com>
2019-05-04 14:20:27 -0600

    write & pass Project model validation specs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks
+
+  validates :name, presence: true, length: { maximum: 30 }
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    User.create({email: 'test@test.com', password: 'test123', first_name: 'John', last_name: 'Doe'})
+  end
+
+  let(:attributes) do
+    {
+      name: 'test@test.com',
+      user_id: 1
+    }
+  end
+
+  let(:missing_name) { attributes.except(:name) }
+  let(:long_name) { attributes.merge(name: 'The Project with the Longest Name') }
+  let(:missing_user) { attributes.except(:user_id) }
+
+
+  context 'name' do
+    it 'should be present' do
+      expect(Project.new(missing_name)).to be_invalid
+    end
+    it 'should be less than 30 characters' do
+      expect(Project.new(long_name)).to be_invalid
+    end
+  end
+
+  it 'should belong to a User' do
+    expect(Project.new(missing_user)).to be_invalid
+  end
+
 end
